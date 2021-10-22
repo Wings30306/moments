c3cc35495b7b9a982d30a71074f02984c985b9ff
Joke Heyndels <jo_hannah@outlook.com>
2021-10-22 14:10:01 +0000

    Use context to change navbar if user is logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
 import { NavLink } from 'react-router-dom'
+import { CurrentUserContext } from '../App'
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext)
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signin">
+                <i className="fas fa-sign-in-alt"></i> Sign In
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signup">
+                <i className="fas fa-user-plus"></i> Sign Up
+            </NavLink>
+        </>)
+    const loggedInIcons = <>{currentUser?.username}</>
+
     return (
         <Navbar expand="md" fixed="top" className={styles.NavBar}>
             <Container>
@@ -15,24 +34,13 @@ const NavBar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink
-                            className={styles.NavLink} 
+                            className={styles.NavLink}
                             activeClassName={styles.Active}
-                            exact 
+                            exact
                             to="/">
-                                <i className="fas fa-home"></i> Home
+                            <i className="fas fa-home"></i> Home
                         </NavLink>
-                        <NavLink 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active} 
-                            to="/signin">
-                                <i className="fas fa-sign-in-alt"></i> Sign In
-                        </NavLink>
-                        <NavLink 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active} 
-                            to="/signup">
-                                <i className="fas fa-user-plus"></i> Sign Up
-                        </NavLink>
+                        { currentUser ? loggedInIcons : loggedOutIcons }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useContext } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -13,36 +13,39 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import axios from "axios";
+import { SetCurrentUserContext } from "../../App";
+
 
 function SignInForm() {
+    const setCurrentUser = useContext(SetCurrentUserContext)
     const [signInData, setSignInData] = useState({
         username: "",
         password: "",
-    })
-    const {username, password} = signInData;
+    });
+    const { username, password } = signInData;
 
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({});
 
     const history = useHistory();
-    
+
     const handleChange = (event) => {
         setSignInData({
             ...signInData,
             [event.target.name]: event.target.value,
         })
-    }
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
             console.log(signInData)
-            await axios.post('/dj-rest-auth/login/', signInData)
+            const {data} = await axios.post('/dj-rest-auth/login/', signInData)
+            setCurrentUser(data.user)
             history.push('/')
-        } catch(err) {
+        } catch (err) {
             setErrors(err.response?.data)
         }
-    }
+    };
 
 
     return (
@@ -53,28 +56,28 @@ function SignInForm() {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
-                            <Form.Control 
-                                className={styles.Input} 
-                                type="text" 
-                                placeholder="Your username" 
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Your username"
                                 name="username"
                                 value={username}
-                                onChange={handleChange}/>
+                                onChange={handleChange} />
                         </Form.Group>
-                        {errors.username?.map((message, idx) => 
+                        {errors.username?.map((message, idx) =>
                             <Alert variant="warning" key={idx}>{message}</Alert>)}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password" onChange={handleChange} value={password}/>
+                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password" onChange={handleChange} value={password} />
                         </Form.Group>
-                        {errors.password?.map((message, idx) => 
+                        {errors.password?.map((message, idx) =>
                             <Alert variant="warning" key={idx}>{message}</Alert>)}
 
                         <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
                             Sign In
                         </Button>
-                        {errors.non_field_errors?.map((message, idx) => 
+                        {errors.non_field_errors?.map((message, idx) =>
                             <Alert variant="warning" key={idx}>{message}</Alert>)}
                     </Form>
 
