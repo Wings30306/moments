6e94a9f9767e6d0cada5bbd4c8de091630aadf59
Joke Heyndels <jo_hannah@outlook.com>
2021-11-03 14:51:54 +0000

    Create Avatar component for reusable avatar images

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Avatar.module.css'
+
+const Avatar = ({ src, height = 45, text}) => {
+    return (
+        <span>
+            <img className={styles.Avatar} src={src} width={height} height={height} alt="Avatar"/>
+            {text}
+        </span>
+    )
+}
+
+export default Avatar
