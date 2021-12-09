b05dca5995825f04deac68b141edead29ea62d92
Joke Heyndels <jo_hannah@outlook.com>
2021-12-09 14:15:13 +0000

    Display comments (unstyled)

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -22,11 +22,13 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
+        console.log(comments)
       } catch (err) {
         console.log(err);
       }
@@ -52,6 +54,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <p key={comment.id}>
+                {comment.owner} : {comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>Be the first to add a comment!</span>
+          ) : (
+            <span>No comments to display</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
