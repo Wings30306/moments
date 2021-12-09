74e016e691634d0fc1904a5d5602f9a9c38b42a7
Joke Heyndels <jo_hannah@outlook.com>
2021-12-09 13:38:48 +0000

    Add CommentCreateForm + css, add into PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,33 +9,49 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostPage() {
   const { id } = useParams();
-  const [ post, setPost ] = useState({ results: [] })
+  const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{data: post}] = await Promise.all([
-            axiosReq.get(`/posts/${id}`)
-        ])
-        setPost({ results: [post]})
-        console.log(post)
-      } catch(err){
-          console.log(err)
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
       }
-    }
+    };
 
     handleMount();
-  }, [id])
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postPage/> 
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
