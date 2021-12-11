ccfb2a0899b290caa7bb99879731433be4a77ca5
Joke Heyndels <jo_hannah@outlook.com>
2021-12-11 15:14:38 +0000

    Finish layout for Profile component

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,26 +1,50 @@
-import React from 'react'
-import styles from "../../styles/Profile.module.css"
-import btnStyles from "../../styles/Button.module.css"
-import Avatar from '../../components/Avatar'
-import { useCurrentUser } from '../../contexts/CurrentUserContext'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
 
-const Profile = ({profile, mobile, imageSize=55}) => {
-    const {id, following_id, owner, image} = profile
+const Profile = ({ profile, mobile, imageSize = 55 }) => {
+  const { id, following_id, owner, image } = profile;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
-    return (
-        <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
-            <div>
-                <Link className="align-self-center" to={`/profiles/${id}`}>
-                    <Avatar src={image} text={owner} width={imageSize}/>
-                </Link>
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} width={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <button 
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              Unfollow
+            </button>
+          ) : (
+            <button 
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              Follow
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
